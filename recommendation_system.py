cab535755b5c1c13301c13c28cb433be7b71a5f3
Russell Feinstein <60637748+RussellFeinstein@users.noreply.github.com>
2023-10-24 16:20:30 -0400

    Add files via upload

---
+++ recommendation_system.py
@@ -0,0 +1,148 @@
+from pyspark.sql import SparkSession
+import re
+import numpy as np
+import sys
+
+# AMAZONREVIEWSFILE = sys.argv[1]
+AMAZONREVIEWSFILE = 'Musical_Instruments_5.json.gz'
+NUMBEROFWORDS = 1000
+productID = 'B0002D01PY'
+
+def buildArray(listOfIndices):
+  returnVal = np.zeros(NUMBEROFWORDS)
+  for index in listOfIndices:
+      returnVal[index] = returnVal[index] + 1
+  mysum = np.sum(returnVal)
+  returnVal = np.divide(returnVal, mysum)
+  return returnVal
+
+def build_one_hot_array (listOfIndices):
+  returnVal = np.zeros (NUMBEROFWORDS)
+  for index in listOfIndices:
+      if returnVal[index] == 0: returnVal[index] = 1
+  return returnVal
+
+def cosSim (x,y):
+	normA = np.linalg.norm(x)
+	normB = np.linalg.norm(y)
+	return np.dot(x,y)/(normA*normB)
+
+def getPrediction(productID, k, tfidfVectors):
+
+  # Get the tf * idf array for the input productID
+  # inputTFIDF = tfidfVectors.filter(lambda x: x[0] == productID).collect()
+  inputTFIDF = tfidfVectors.takeSample(False, 1, seed=0)
+
+  # Get the distance from the input text string to all database documents,
+  # using cosine similarity
+  distances = tfidfVectors.map(lambda x: (x[0], cosSim(x[1], inputTFIDF[0][1])))
+
+  # Get top k recommended items
+  topK = distances.top(k, lambda x: x[1])[1:]
+
+  # Strip similarity score, just return productIDs
+  topK = list(zip(*topK))[0]
+
+  return topK, inputTFIDF[0][0]
+
+def main():
+  # Create Spark Session
+  spark = SparkSession.builder.appName('sparkdf').getOrCreate()
+  sc = spark.sparkContext
+
+  # Read data
+  allReviewData = spark.read.json(AMAZONREVIEWSFILE).rdd
+  
+  # Get number of reviews
+  numDocs = allReviewData.count()
+
+  # Get just IDs and review text
+  # (productID, text)
+  idAndText = allReviewData.map(lambda x: (x[0], x[3]))
+
+  # Now, we split the text in each (docID, text) pair into a list of words
+  # After this step, we will have a data set with
+  # (docID, ["word1", "word2", "word3", ...])
+
+  # Remove all non alpha characters, make them lowercase, and split on spaces
+  regex = re.compile('[^a-zA-Z]')
+  idAndTermsList = idAndText\
+                      .map(lambda x : (x[0], regex.sub(' ', x[1])\
+                      .lower().split()))
+
+  idAndTermsList = idAndTermsList.reduceByKey(lambda x,y: x+y)
+
+  # Now get the top NUMBEROFWORDS terms
+  # 
+  # first change (docID, ["word1", "word2", "word3", ...])
+  # to ("word1", 1) ("word2", 1)
+  termOnePairs = idAndTermsList.flatMap(lambda x: x[1]).map(lambda x: (x, 1))
+
+  # Now, count all of the terms, giving us ("word1", 1433), ("word2", 3423423), etc.
+  allCounts = termOnePairs.reduceByKey(lambda x,y: x+y)
+
+  # Get the top NUMBEROFWORDS terms in a local array in a sorted format based on frequency 
+  topTerms = allCounts.takeOrdered(NUMBEROFWORDS, lambda x: -x[1])
+
+  # print("Top 100 Terms in Corpus:")
+  # for x in topTerms[:100]:
+  #   print(x)
+
+  # We'll create a RDD that has a set of (word, dictNum) pairs
+  # start by creating an RDD that has the number 0 through 1000
+  # 1000 is the number of terms that will be in our dictionary
+  kZerosArray = sc.parallelize(range(NUMBEROFWORDS))
+
+  # Now, we transform (0), (1), (2), ... to ("MostCommonWord", 1)
+  # ("NextMostCommon", 2), ...
+  # the number will be the spot in the dictionary used to tell us
+  # where the word is located
+  dictionary = kZerosArray.map(lambda x : (topTerms[x][0], x))
+
+
+
+  # Next create TF vectors
+
+  # Next, we get a RDD that has, for each (docID, ["word1", "word2", "word3", ...]),
+  # ("word1", docID), ("word2", docId), ...
+  termID_pairs = idAndTermsList.flatMap(lambda x: ((j, x[0]) for j in x[1]))
+
+  # # Now join and link them, to get a set of ("word1", (dictionaryPos, docID)) pairs
+  termOnePairs = dictionary.join(termID_pairs)
+
+  # Now, we drop the actual word itself to get a set of (docID, dictionaryPos) pairs
+  docPosPairs = termOnePairs.map(lambda x: (x[1][1], [x[1][0]]))
+
+  # Now get a set of (docID, [dictionaryPos1, dictionaryPos2, dictionaryPos3...]) pairs
+  termsInEachDoc = docPosPairs.reduceByKey(lambda x,y: x+y)\
+                                    .map(lambda x: (x[0], sorted(x[1])))                                    
+
+  # Convert list to numpy array
+  termFreq = termsInEachDoc.map(lambda x: (x[0], buildArray(x[1])))
+
+  # Create a OneHot encoding 
+  oneHot = termsInEachDoc.map(lambda x: (x[0], build_one_hot_array(x[1])))
+
+  # Now, add up all of those arrays into a single array, where the
+  # i^th entry tells us how many
+  # individual documents the i^th word in the dictionary appeared in
+  docFreq = oneHot.reduce(lambda x, y: ("", np.add(x[1], y[1])))[1]
+
+  # # Create an array of NUMBEROFWORDS entries, each entry with the value numberOfDocs (number of docs)
+  # multiplier = np.full(NUMBEROFWORDS, numDocs)
+
+  # Get the version of dfArray where the i^th entry is the inverse-document frequency for the
+  # i^th word in the corpus
+  inverseDocFreq = np.log(np.divide(np.full(NUMBEROFWORDS, numDocs), docFreq))
+
+  # Finally, convert all of the tf vectors in termFreq to tf * idf vectors
+  tfidfVectors = termFreq.map(lambda x: (x[0],np.multiply(x[1],inverseDocFreq)))
+
+  # Test
+  pred, item = getPrediction(productID, 20, tfidfVectors)
+  print('Product recommendations for:', item)
+  for x in pred:
+    print(x)
+
+if __name__ == '__main__':
+  main()
---
+++ sentiment_analysis.py
@@ -0,0 +1,162 @@
+from pyspark.sql import SparkSession
+import nltk
+from nltk.stem import WordNetLemmatizer
+from nltk.sentiment.vader import SentimentIntensityAnalyzer
+import string
+
+def
+  from nltk.corpus import stopwords
+
+def removeStopWordsFunct(x):
+    stop_words=set(stopwords.words('english'))
+    filteredSentence = [w for w in x if not w in stop_words]
+    return filteredSentence
+
+
+def removePunctuationsFunct(x):
+    list_punct=list(string.punctuation)
+    filtered = [''.join(c for c in s if c not in list_punct) for s in x] 
+    filtered_space = [s for s in filtered if s] #remove empty space 
+    return filtered_space
+
+
+def lemmatizationFunct(x):
+    nltk.download('wordnet')
+    lemmatizer = WordNetLemmatizer()
+    finalLem = [lemmatizer.lemmatize(s) for s in x]
+    return finalLem
+
+
+def joinTokensFunct(x):
+    x = " ".join(x)
+    return x
+
+
+#extracting the keywords
+def extractphraseFunct(x):
+    
+    stop_words=set(stopwords.words('english'))
+    def leaves(tree):
+        #Finds NP (nounphrase) leaf nodes of a chunk tree
+        for subtree in tree.subtrees(filter = lambda t: t.label()=='NP'):
+            yield subtree.leaves()
+    def acceptable_word(word):
+        """Checks conditions for acceptable word: length, stopword. We can increase the length if we want to consider large phrase"""
+        accepted = bool(3 < len(word) <= 20
+            and 'https' not in word.lower()
+            and 'http' not in word.lower()
+            and '#' not in word.lower()
+            )
+        yield accepted
+    def get_terms(tree):
+        for leaf in leaves(tree):
+            term = [w for w,t in leaf if not w in stop_words if acceptable_word(w)]
+            yield term
+    sentence_re = r'(?:(?:[A-Z])(?:.[A-Z])+.?)|(?:\w+(?:-\w+)*)|(?:\$?\d+(?:.\d+)?%?)|(?:...|)(?:[][.,;"\'?():-_`])'
+    grammar = r"""
+    NBAR:
+        {<NN.*|JJ>*<NN.*>}  # Nouns and Adjectives, terminated with Nouns
+
+    NP:
+        {<NBAR>}
+        {<NBAR><IN><NBAR>}  # Above, connected with in/of/etc...
+    """
+    chunker = nltk.RegexpParser(grammar)
+    tokens = nltk.regexp_tokenize(x,sentence_re)
+    postoks = nltk.tag.pos_tag(tokens) #Part of speech tagging
+    tree = chunker.parse(postoks) #chunking
+    terms = get_terms(tree)
+    temp_phrases = []
+    for term in terms:
+        if len(term):
+            temp_phrases.append(' '.join(term))
+
+    finalPhrase = [w for w in temp_phrases if w] #remove empty lists
+    return finalPhrase
+
+
+#sentiment of each key phrase
+def sentimentWordsFunct(x):
+    
+    analyzer = SentimentIntensityAnalyzer() 
+    senti_list_temp = []    
+    for i in x:
+        y = ''.join(i) 
+        vs = analyzer.polarity_scores(y)
+        senti_list_temp.append((y, vs))
+        senti_list_temp = [w for w in senti_list_temp if w]    
+    sentiment_list  = []
+    for j in senti_list_temp:
+        first = j[0]
+        second = j[1]
+    
+        for (k,v) in second.items():
+            if k == 'compound':
+                if v < 0.0:
+                    sentiment_list.append((first, "Negative"))
+                elif v == 0.0:
+                    sentiment_list.append((first, "Neutral"))
+                else:
+                    sentiment_list.append((first, "Positive"))     
+    
+    return sentiment_list
+
+
+def main():
+
+    spark = SparkSession.builder.appName("project").getOrCreate()
+
+    output = "TermProjectResults"
+    df = spark.read.json('Musical_Instruments_5.json')
+    df.show(5)
+
+
+
+    # text preprocessing
+    reviews_rdd = df.select("reviewText").rdd.flatMap(lambda x: x)
+    lowerCase_sentRDD = reviews_rdd.map(lambda x : x.lower())
+    sentenceTokenizeRDD = lowerCase_sentRDD.map(lambda x: nltk.sent_tokenize(x))
+    wordTokenizeRDD = sentenceTokenizeRDD.map(lambda x: [word for line in x for word in line.split()])
+
+
+
+    stopwordRDD = wordTokenizeRDD.map(removeStopWordsFunct)
+    rmvPunctRDD = stopwordRDD.map(removePunctuationsFunct)
+    lem_wordsRDD = rmvPunctRDD.map(lemmatizationFunct)
+    joinedTokens = lem_wordsRDD.map(joinTokensFunct)
+
+    extractphraseRDD = joinedTokens.map(extractphraseFunct)
+
+
+
+
+
+    sentimentRDD = extractphraseRDD.map(sentimentWordsFunct)
+    pos_sentimentRDD = sentimentRDD.flatMap(lambda list: list).filter(lambda y:y[1]=="Positive")
+    neg_sentimentRDD = sentimentRDD.flatMap(lambda list: list).filter(lambda y:y[1]=="Negative")
+    neutral_sentimentRDD = sentimentRDD.flatMap(lambda list: list).filter(lambda y:y[1]=="Neutral")
+
+    freqDistRDD = neutral_sentimentRDD.map(lambda x: (x[0], 1)).reduceByKey(lambda x,y : x+y).sortBy(lambda x: x[1], ascending = False)
+
+    df_fDist = freqDistRDD.toDF() #converting RDD to spark dataframe
+    df_fDist.createOrReplaceTempView("myTable") 
+    df2 = spark.sql("SELECT _1 AS Keywords, _2 as Frequency from myTable limit 20")
+    df2.coalesce(1).write.mode("overwrite").options(header=True).csv(output + "/neutral")
+
+    freqDistRDD = pos_sentimentRDD.map(lambda x: (x[0], 1)).reduceByKey(lambda x,y : x+y).sortBy(lambda x: x[1], ascending = False)
+
+    df_fDist = freqDistRDD.toDF() #converting RDD to spark dataframe
+    df_fDist.createOrReplaceTempView("myTable") 
+    df2 = spark.sql("SELECT _1 AS Keywords, _2 as Frequency from myTable limit 20") #renaming columns 
+    df2.coalesce(1).write.mode("overwrite").options(header=True).csv(output + "/positive")
+
+    freqDistRDD = neg_sentimentRDD.map(lambda x: (x[0], 1)).reduceByKey(lambda x,y : x+y).sortBy(lambda x: x[1], ascending = False)
+
+    df_fDist = freqDistRDD.toDF() #converting RDD to spark dataframe
+    df_fDist.createOrReplaceTempView("myTable") 
+    df2 = spark.sql("SELECT _1 AS Keywords, _2 as Frequency from myTable limit 20") #renaming columns 
+    df2.coalesce(1).write.mode("overwrite").options(header=True).csv(output + "/negative")
+
+
+if __name__ == '__main__':
+    main()
